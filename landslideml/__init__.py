824d48196bccf59ab32121c6148d14c80c2aa410
Victor Improta <33900828+ImProta@users.noreply.github.com>
2024-07-08 00:20:32 +0200

    Added the requirements file and modified __init__.py to account for the modules reader.py and model.py

--- landslideml/__init__.py
+++ landslideml/__init__.py
@@ -2,3 +2,6 @@
 Basic package for landslide susceptibility modeling using machine learning.
 """
 VALID_MODELS = ['RandomForest', 'SVM', 'GBM']
+
+from .reader import generate_model
+from .model import MlModel
